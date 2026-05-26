38b86e6335f7c7061d6640fee8541421d503c0d5
raghav9739693 <93114257+raghav9739693@users.noreply.github.com>
2026-05-26 06:50:47 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/",   // ✅ IMPORTANT FIX
+
   plugins: [react()],
+
   server: {
     host: '0.0.0.0',
     port: 5173
